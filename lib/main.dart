ea45f8d582ad20be4d5e63bfe1c7e99486f305b9
Razzle5 <althafarros05@gmail.com>
2025-10-15 10:05:25 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:job_app/features/user_dashboard/user_dasboard_screen.dart';
+import 'package:job_app/features/user_dashboard/Home_Screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Job App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: user_dasboard_screen(),
+      debugShowCheckedModeBanner: false,
+      home: const NavBar(),
     );
   }
 }
