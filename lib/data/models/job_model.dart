95e3e639c5e7978457d383902690bf9b0d12332f
Razzle5 <althafarros05@gmail.com>
2025-10-25 22:50:25 +0700

    update api data

--- lib/data/models/job_model.dart
+++ lib/data/models/job_model.dart
@@ -1,1 +1,40 @@
-//job model
+class JobModel {
+  final int id;
+  final int companyId;
+  final String title;
+  final String description;
+  final String location;
+  final String salary;
+  final String type;
+  final String companyName; 
+  final String companyLogo;
+  
+  JobModel({
+    required this.id,
+    required this.companyId,
+    required this.title,
+    required this.description,
+    required this.location,
+    required this.salary,
+    required this.type,
+    required this.companyName,
+    required this.companyLogo,
+  });
+
+  factory JobModel.fromJson(Map<String,dynamic>json){
+    final companyData = json['company'] as Map<String, dynamic>?;
+
+    return JobModel(
+      id : json['id'] as int,
+      companyId: json['company_id'] as int,
+      title: json['title'] as String,
+      description: json['description'] as String,
+      location: json['location'] as String,
+      salary: json['salary'] as String,
+      type: json['type'] as String,
+
+      companyName: companyData?['brand_name'] ?? 'N/A', 
+      companyLogo: 'Iconsax.facebook',
+    );
+  }
+}
--- lib/data/repositories/job_repository.dart
+++ lib/data/repositories/job_repository.dart
@@ -1,1 +1,26 @@
-//job repo
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:job_app/data/models/job_model.dart';
+import 'package:job_app/constants/api_constants.dart';
+
+class JobRepository{
+  Future<List<JobModel>> fetchJobs() async{
+    final url = Uri.parse('${ApiConstants.tBaseUrl}/api/jobs');
+
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200){
+        final body = jsonDecode(response.body);
+        final jobListJson = body['jobs'] as List;
+        return jobListJson
+          .map((jobJson) => JobModel.fromJson(jobJson as Map<String, dynamic>))
+          .toList();
+      } else {
+        throw Exception('Gagal memuat pekerjaan, Status code : ${response.statusCode}');
+      }
+    } catch (e){
+      throw Exception('Gagal koneksi ke server: $e');
+    }
+  }
+}
